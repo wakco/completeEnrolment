78d1e9c81dfb6aa5a24725cccec18ac817ae01f6
Richard <xplora@wak.co.nz>
2025-07-01 17:10:41 +1200

    working through it

--- completeEnrolment.sh
+++ completeEnrolment.sh
@@ -1,21 +1,28 @@
 #!/bin/zsh -f
 
-completeEnrolment="/usr/local/bin/completeEnrolment"
-mkdir -p $( dirname $completeEnrolment )
-head -n1 "$0" > "$completeEnrolment"
-chmod -R 755 $( dirname $completeEnrolment )
-chown -R root:wheel $( dirname $completeEnrolment )
-tail -n $(($( wc -l "$0" | awk '{ print $1 }' )-$( grep -n -m1 "^exit 0" "$0" | cut -d ':' -f 1 ))) "$0" >> "$completeEnrolment"
+# Commands
+# For anything outside /bin /usr/bin, /sbin, /usr/sbin
 
-exit 0
-#
-#
+C_JAMF="/usr/local/bin/jamf"
+C_INSTALL="/usr/local/Installomator/Installomator.sh"
+C_DIALOG="/usr/local/bin/dialog"
+C_MKUSER="/usr/local/bin/mkuser"
+C_ENROLMENT="/usr/local/bin/completeEnrolment"
+
+# Variables
+
+DEFAULTS_NAME="completeEnrolment"
+DEFAULTS_PLIST="$DEFAULTS_NAME.plist"
+LIB="/Library"
+DEFAULTS_FILE="$LIB/Managed Preferences/$DEFAULTS_PLIST"
+LOGIN_PLIST="$LIB/LaunchAgents/$DEFAULTS_PLIST"
+STARTUP_PLIST="$LIB/LaunchDaemons/$DEFAULTS_PLIST"
 
 # Whose logged in
 
-# The who command method is used as it is the only one that still returns _mbsetupuser as used by
+# The who command method is used as it is the only one that reliably returns _mbsetupuser as used by
 # Setup Assistant, confirming it as the only username logged into the console is the best way to
-# identify a ADE/DEP Jamf PreStage Enrollment (after confirming the script was started by Jamf Pro
+# identify an ADE/DEP Jamf PreStage Enrollment (after confirming the script was started by Jamf Pro
 # with a / in $1).
 
 if [ "$( who | grep console | wc -l )" -gt 1 ]; then
@@ -24,8 +31,84 @@ else
  WHO_LOGGED="$( who | grep -m1 console | cut -d " " -f 1 )"
 fi
 
+LOG_FILE="$LIB/Logs/$DEFAULTS_NAME-$( if [ "$1" = "/" ]; then echo "$WHO_LOGGED" ; else echo "$1" ; fi )-$( date "+%Y-%m-%d %H-%M-%S %Z" ).log"
+
+# Functions
+
+logIt() {
+ echo "$(date) - $@" 2>&1 | tee -a "$LOG_FILE"
+}
+
+runIt() {
+ eval "$@" 2>&1 | tee -a "$LOG_FILE"
+}
+
+defaultRead() {
+ defaults read "$DEFAULTS_FILE" "$1" 2>/dev/null
+}
+
+myInstall() {
+ until [ -e "$1" ]; do
+  case $2 in
+   policy)
+    runIt "/usr/local/bin/jamf policy -event $3"
+   ;;
+   install)
+    runIt "/usr/local/Installomator/Installomator.sh $3 NOTIFY=silent GITHUBAPI=$GITHUB_API"
+   ;;
+   *)
+    logIt "Error: myInstall: \$2 must be either policy or install, soft failing this install attempt by touching the check file"
+    touch "$1"
+   ;;
+  esac
+  if [ ! -e "$1" ]; then
+   sleep 5
+  fi
+ done
+}
+
+# Lets get started
+
+until [ -e "$DEFAULTS_FILE" ]; do
+ sleep 1
+done
+
 case $1 in
  /)
+  # set time
+  SYSTEM_TIME_ZONE="${"$( defaultRead systemTimeZone )":-"$( systemsetup -gettimezone | awk '{print $NF}' )"}"
+  #"# this comment fixes an Xcode display bug
+  SYSTEM_TIME_SERVER="${"$( defaultRead systemTimeServer )":-"$( systemsetup -getnetworktimeserver | awk '{print $NF}' )"}"
+  #"# this comment fixes an Xcode display bug
+  runIt "/usr/sbin/systemsetup -settimezone '"$SYSTEM_TIME_ZONE"'"
+  sleep 5
+  runIt "/usr/sbin/systemsetup -setnetworktimeserver $SYSTEM_TIME_SERVER"
+  sleep 5
+  runIt "/usr/bin/sntp -Ss $SYSTEM_TIME_SERVER"
+  sleep 1
+
+  # Install completeEnrolment
+  logIt "Installing $C_ENROLMENT..."
+  ditto "$0" "$C_ENROLMENT"
+  
+  # Install Rosetta (just in case, and account for it being missing in macOS 28+)
+  if [ "$( arch )" = "arm64" ] && [ $(sw_vers -productVersion | cut -d '.' -f 1) -lt 28 ]; then
+   logIt "Installing Rosetta on Apple Silicon..."
+   runIt "/usr/sbin/softwareupdate --install-rosetta --agree-to-license"
+  fi
+  
+  
+
+  # Setup Login Window
+  defaults write "$LOGIN_DIALOG_PLIST" LimitLoadToSessionType -array "LoginWindow"
+  defaults write "$LOGIN_DIALOG_PLIST" Label "$DEFAULTS_NAME.loginwindow"
+  defaults write "$LOGIN_DIALOG_PLIST" RunAtLoad -bool TRUE
+  defaults write "$LOGIN_DIALOG_PLIST" ProgramArguments -array "$C_ENROLMENT" "loginWindow"
+  chmod ugo+r "$LOGIN_DIALOG_PLIST"
+  
+  
+  launchctl load -S LoginWindow "$LAUNCHAGENT"
+  
   # Executed by Jamf Pro
   # Load config profile settings and save them for later use in a more secure location, do the same
   # for supplied options such as passwords, only attempt to store them in the Keychain (such as
@@ -47,7 +130,7 @@ case $1 in
  *)
   # load saved settings
  ;|
- atLoginWindow)
+ loginWindow)
   # triggered loginwindow
  ;; # or ;& ?
  startProcessing)
